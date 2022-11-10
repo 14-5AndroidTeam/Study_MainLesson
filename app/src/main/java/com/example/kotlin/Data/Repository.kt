f24dc76d8cc4dff4465ece322c7b86cc0035f829
이용인 <yongin0517@gmail.com>
2022-11-10 12:19:46 +0900

    Feat:"추가 - 서버 요청 및 구현에 필요한 파일"

---
+++ app/src/main/java/com/example/kotlin/Data/Repository.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin.Data
+
+import android.util.Log
+import com.example.kotlin.Data.DTO.ToDoList
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Repository {
+    /**데이터베이스에 접근할 수 있는 곳*/
+    private val Dr = DataResource
+    fun getList(param: GetDataCallback<ToDoList>) {
+        val call = Dr.service.getList() //api 호출
+
+        /**비동기 통신: enqueue*/
+        call.enqueue(object : Callback<ToDoList>{
+            override fun onResponse(call: Call<ToDoList>, response: Response<ToDoList>) {
+                /**응답 성공*/
+                /**Viewmodel의 인터페이스 구현부에 데이터 넘겨주기*/
+                param.onSuccess(response.body())
+            }
+
+            override fun onFailure(call: Call<ToDoList>, t: Throwable) {
+                /**응답 실패*/
+                param.onFailure(t)
+            }
+        })
+    }
+
+    /**서버응답 결과를 뷰모델로 넘겨줄 수 있는 인터페이스*/
+    interface GetDataCallback<T>{
+        fun onSuccess(data:T?)
+        fun onFailure(throwable: Throwable)
+    }
+}
---
+++ app/src/main/java/com/example/kotlin/Data/Service.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin.Data
+
+import com.example.kotlin.Data.DTO.ToDoList
+import retrofit2.Call
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface Service {
+    /**GET, POST 등의 API 정의하기*/
+    @GET("list/") //todolist 불러오는 api
+    fun getList(): Call<ToDoList>
+}
